3df0f218943db8f1ff0fd13a584b127284ceee2c
Nah-Null <pgthman@gmail.com>
2025-02-06 13:38:30 +0700

    Update User.java

--- src/main/java/com/wed/app/model/User.java
+++ src/main/java/com/wed/app/model/User.java
@@ -20,7 +20,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int id;
-    
     private String name;
     private String email;
     private String password;
